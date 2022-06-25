3d153987ac06dc21edac98775385d150abbbdecf
ccdale <chris.allison@hivehome.com>
2022-06-25 10:43:51 +0100

    remove nasties, replace -_- with _ and _-_ with -

--- tvrecorder/strings.py
+++ tvrecorder/strings.py
@@ -6,14 +6,16 @@ from ccaerrors import errorNotify
 def cleanString(istring):
     """Replaces 'nasty' chars in strings that could be used as filenames"""
     try:
-        replace = "/(){}@~!£$%^&*+='\""
-        for i in replace:
+        remove = "/(){}@~!£$%^&*+='\""
+        for i in remove:
             istring = istring.replace(i, "")
         istring = istring.replace(" ", "_")
         while "__" in istring:
             istring = istring.replace("__", "_")
         while "--" in istring:
             istring = istring.replace("--", "-")
+        istring = istring.replace("_-_", "-")
+        istring = istring.replace("-_-", "_")
         return istring
     except Exception as e:
         errorNotify(sys.exc_info()[2], e)
