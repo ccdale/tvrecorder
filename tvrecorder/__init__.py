dd9b9ff6a32741589a8f30f8effc9e0c6c4abc50
ccdale <chris.allison@hivehome.com>
2022-06-30 11:21:55 +0100

    Bumping version from 0.1.34 to 0.1.35

--- tvrecorder/__init__.py
+++ tvrecorder/__init__.py
@@ -16,5 +16,5 @@
 #     You should have received a copy of the GNU General Public License
 #     along with tvrecorder.  If not, see <http://www.gnu.org/licenses/>.
 #
-__version__ = "0.1.34"
+__version__ = "0.1.35"
 __appname__ = "tvrecorder"
