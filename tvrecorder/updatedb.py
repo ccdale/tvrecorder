08e50a78ef6a86410dc23414eaf7f996cf0c097a
ccdale <chris.allison@hivehome.com>
2022-06-30 11:22:50 +0100

    pluralised function names (dick head)

--- tvrecorder/updatedb.py
+++ tvrecorder/updatedb.py
@@ -27,7 +27,7 @@ from tvrecorder.credential import getSDCreds
 from tvrecorder.config import Configuration
 from tvrecorder.db import makeDBEngine
 from tvrecorder.sdapi import SDApi
-from tvrecorder.wrangler import updateChannels, scheduleMd5
+from tvrecorder.wrangler import updateChannels, schedulesMd5
 
 ccalogging.setConsoleOut()
 log = ccalogging.log
@@ -83,7 +83,7 @@ def updatedb():
         debug = True
         cf, sd, mysqleng = begin(__appname__, debug=debug)
         linupRefresh(sd, cf, mysqleng)
-        scheduleMd5(sd)
+        schedulesMd5(sd)
         close()
     except Exception as e:
         errorNotify(sys.exc_info()[2], e)
